38e54956754aa6721c49bcdf279772fc6948c357
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2023-11-03 11:56:56 -0400

    Visitors: * Update SimplifiedFileVisitor - implement method preVisitDirectory

--- visitors/src/main/java/files/jvm/visitors/SimplifiedFileVisitor.java
+++ visitors/src/main/java/files/jvm/visitors/SimplifiedFileVisitor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
 
 /** Overrides the FileVisitor and provides default implementations for methods not commonly needed.
  */
@@ -11,20 +12,31 @@ public abstract class SimplifiedFileVisitor
 	implements FileVisitor<Path> {
 
 	@Override
+	public FileVisitResult preVisitDirectory(
+		Path path,
+		BasicFileAttributes basicFileAttributes
+	) {
+		return path != null ? FileVisitResult.CONTINUE
+			: FileVisitResult.SKIP_SUBTREE;
+	}
+
+	@Override
 	public FileVisitResult visitFileFailed(
 		Path path,
 		IOException exception
-	) {
+	) throws IOException {
+		if (exception != null)
+			throw exception;
 		return FileVisitResult.TERMINATE;
 	}
 
 	@Override
 	public FileVisitResult postVisitDirectory(
 		Path path,
-		IOException e
+		IOException exception
 	) throws IOException {
-		if (e != null)
-			throw e;
+		if (exception != null)
+			throw exception;
 		return FileVisitResult.CONTINUE;
 	}
 
